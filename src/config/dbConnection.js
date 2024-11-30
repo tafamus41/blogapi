8473eed17e9da585c92f884144c2fc34d09512fc
asimov <seyranmustafa4@gmail.com>
2024-11-30 22:15:31 +0300

    p

---
+++ src/config/dbConnection.js
@@ -0,0 +1,13 @@
+"use strict"
+
+/* -------------------------------------------------------------------------- */
+/*                    EXPRESSJS-BLOG Project with Mongoose                    */
+/* -------------------------------------------------------------------------- */
+
+const mongoose=require("mongoose")
+const {CustomError}=require("../errors/customError")
+
+/* -------------------------------------------------------------------------- */
+const dbConnection=()=>{
+    
+}
