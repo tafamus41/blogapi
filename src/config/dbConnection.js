3e690bed494ab0fe67f5c917887cbf6a73c1e12f
asimov <seyranmustafa4@gmail.com>
2024-11-30 23:05:51 +0300

    p

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -39,5 +39,6 @@ class DatabaseConnection {
         });
     }
   }
+    module.exports= new DatabaseConnection()
  /* -------------------------------------------------------------------------- */ 
 module.exports=dbConnection
