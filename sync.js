647486fbd2595c8f70c31a408365bbbff81cb8b5
asimov <seyranmustafa4@gmail.com>
2024-12-09 04:23:29 +0300

    p

--- sync.js
+++ sync.js
@@ -13,9 +13,9 @@ module.exports = async () => {
 
     /* Exampla Data */
     // Deleted All Records:
-    await User.deleteMany().then(() => console.log(' - User Deleted All'))
-    await BlogCategory.deleteMany().then(() => console.log(' - BlogCategory Deleted All'))
-    await BlogPost.deleteMany().then(() => console.log(' - BlogPost Deleted All'))
+    // await User.deleteMany().then(() => console.log(' - User Deleted All'))
+    // await BlogCategory.deleteMany().then(() => console.log(' - BlogCategory Deleted All'))
+    // await BlogPost.deleteMany().then(() => console.log(' - BlogPost Deleted All'))
 
     // Example User:
     const user = await User.create({
