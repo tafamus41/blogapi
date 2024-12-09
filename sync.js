2b2a604f18afbb1f7d91b1551887cdc178717dcc
asimov <seyranmustafa4@gmail.com>
2024-12-09 04:29:31 +0300

    p

--- sync.js
+++ sync.js
@@ -13,7 +13,7 @@ module.exports = async () => {
 
     /* Exampla Data */
     // Deleted All Records:
-    // await User.deleteMany().then(() => console.log(' - User Deleted All'))
+    await User.deleteMany().then(() => console.log(' - User Deleted All'))
     // await BlogCategory.deleteMany().then(() => console.log(' - BlogCategory Deleted All'))
     // await BlogPost.deleteMany().then(() => console.log(' - BlogPost Deleted All'))
 
